eccf6782ef444b585581a3b4785fae4af8c9ef76
Runwill <1798273016@qq.com>
2025-04-22 15:01:49 +0800

    cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // 引入 CORS 模块
 const routes = require('./routes/index');
 
 const app = express();
+
+// 启用 CORS
+app.use(cors({
+    origin: 'http://127.0.0.1:5500', // 允许的前端地址
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // 允许的 HTTP 方法
+    credentials: true // 如果需要发送 Cookie 或认证信息
+}));
 
 // Middleware
 app.use(bodyParser.json());
